bf4c3b2b7b837666cc8ffa50146d881a94bf733e
Harshit Dubey <dubeyharshi538@gmail.com>
2024-11-28 00:51:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/live_weather_info/', // match this with your GitHub Pages repository name
+  plugins: [/* your plugins here */],
+});
